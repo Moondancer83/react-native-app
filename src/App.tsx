3a8e3ac54f8bc3b00d35a353330f195dcaf0ce9a
Mark Kalinovits <kalee.mark@gmail.com>
2020-07-03 23:35:43 +0200

    Create first screen

--- src/App.tsx
+++ src/App.tsx
@@ -8,78 +8,47 @@
  * @format
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-declare const global: {HermesInternal: null | {}};
+import React, {useState} from 'react';
+import {StatusBar, StyleSheet, Text, TouchableOpacity, View,} from 'react-native';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Text>Hello world</Text>
-        </ScrollView>
-      </SafeAreaView>
+      <StatusBar barStyle="default" />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+        <TouchableOpacity onPress={() => {setCount(count + 1)}} style={style.button}>
+          <Text style={style.buttonText}>PRESS ME</Text>
+        </TouchableOpacity>
+        <Text style={style.counterText}>{count}</Text>
+
+      </View>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+const style = StyleSheet.create({
+  button: {
+    padding: 10,
+    borderWidth: 3,
+    borderRadius: 5,
+    backgroundColor: "lightgreen",
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  buttonText: {
+    fontSize: 48,
+    fontWeight: "bold"
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  counterText: {
+    color: "red",
+    marginTop: 40,
+    fontSize: 36
+  }
 });
 
 export default App;
